5634cd12cdd582aa0ca17bd37c8045adade9df84
Rafiul Alam <rafiul7368@gmail.com>
2024-02-19 18:40:30 +0600

    js add

--- main.js
+++ main.js
@@ -1,1 +1,111 @@
-console.log("Hello");
+
+
+let left=40
+let book=0
+let ar=[]
+function getPrice(id){
+
+    if(ar.includes(id)){
+       
+        
+    }
+    
+    else{
+        
+        if(ar.length<4){
+            ar.push(id)
+            let element=document.getElementById(id)
+            element.style.backgroundColor='Green'
+            element.style.color='white'
+            left=left-1
+            let leftSeat=document.getElementById("left-seat")
+            leftSeat.innerText=`${left}`
+            book=book+1
+            let bookSeat=document.getElementById("book-seat")
+            bookSeat.innerText=`${book}`
+            let addPro=document.getElementById("add-pro")
+            let newpro=document.createElement("p")
+            newpro.innerText=`${id} --------------- Economy --------- 550`
+            addPro.append(newpro)
+            priceSum()
+            
+        }
+    }
+    
+}
+
+
+let total=0
+function priceSum(){
+
+    total=total+550
+    console.log(total);
+
+    let totalPrice=document.getElementById('total-price')
+    totalPrice.innerText=`${total}`
+    let grand=document.getElementById('grand')
+    grand.innerText=`${total}`
+    
+}
+
+
+function cupon(){
+    let getCupon=document.getElementById("cup")
+    if(getCupon.value==='NEW15' && book===4){
+        let dis=(15/100)*total
+        let disPrice=document.getElementById("dis-price")
+        let appdisPrice=document.createElement('p')
+        appdisPrice.innerHTML=`<h1 class="font-inter font-medium text-base pt-1">Discount &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; &nbsp;  BDT <span id="total-price">${dis}</span></h1>`
+        disPrice.append(appdisPrice)
+        let haowa=document.getElementById("haowa")
+        haowa.classList.add('hidden')
+        let grand=document.getElementById('grand')
+        grand.innerText=`${total-dis}`
+
+    }
+    else if(getCupon.value=== 'Couple 20' && book===4){
+        let dis=(20/100)*total
+        let disPrice=document.getElementById("dis-price")
+        let appdisPrice=document.createElement('p')
+        appdisPrice.innerHTML=`<h1 class="font-inter font-medium text-base pt-1">Discount &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; &nbsp;  BDT <span id="total-price">${dis}</span></h1>`
+        disPrice.append(appdisPrice)
+        let haowa=document.getElementById("haowa")
+        haowa.classList.add('hidden')
+        let grand=document.getElementById('grand')
+        grand.innerText=`${total-dis}`
+    }
+
+    else{
+
+        getCupon.value=''
+        alert("Enter valid coupon")
+        
+    }
+}
+
+
+// let nextButton=document.getElementById('next-button')
+// nextButton.disabled=false
+// let phone=document.getElementById('phone')
+// console.log(phone.value);
+// if(left>36 && phone.value!==''){
+//     nextButton.disabled=false
+// }
+
+function next(){
+    let nextButton=document.getElementById('main')
+    nextButton.classList.add('hidden')
+    let success=document.getElementById('suc')
+    success.classList.remove('hidden')
+
+
+
+
+}
+
+function con(){
+    let main=document.getElementById('main')
+    main.classList.remove('hidden')
+    let success=document.getElementById('suc')
+    success.classList.add('hidden')
+}
